68fc681ab480d01d84f1ae9badffd9e09177ff1a
HARSHVEER SINGH <harsh.official642007@gmail.com>
2025-10-08 23:11:10 +0530

    Add files via upload

---
+++ Day ---- 21/Question 42.c
@@ -0,0 +1,24 @@
+// Q42: Write a program to check if a number is a perfect number.
+
+
+#include<stdio.h>
+void main()
+{
+    int num,sum=0;
+    printf("enter a number = ");
+    scanf("%d",&num);
+    for(int i=1;i<num;i++)
+    {
+        if(num%i==0)
+        {
+            sum+=i;
+        }
+    }
+    if(sum==num)
+    {
+        printf("perfect number");
+    }
+    else{
+        printf("not a perfect number");
+    }
+}
