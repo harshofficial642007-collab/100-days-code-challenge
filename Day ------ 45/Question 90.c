79532895a93a7744c99a5eed4d590b568bbc4e97
Harshveer_Singh <harsh.official642007@gmail.com>
2025-12-22 01:47:48 +0530

    Add files via upload

---
+++ Day ------ 45/Question 90.c
@@ -0,0 +1,22 @@
+// Q90: Toggle case of each character in a string.
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100];
+    printf("enter a string\t");
+    fgets(str,50,stdin);
+    for(int i=0;i<strlen(str);i++)
+    {
+        if(str[i]>='A'&&str[i]<='Z')
+        {
+            str[i]=str[i]+32;
+        }
+        else{
+            str[i]=str[i]-32;
+        }
+    }
+    printf("%s",str);
+    return 0;
+}
---
+++ Day ------ 46/Question 92.c
@@ -0,0 +1,22 @@
+// Q92: Find the first repeating lowercase alphabet in a string.
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+     char str[100];
+    printf("enter a string\t");
+    fgets(str,50,stdin);
+    for(int i=0;i<strlen(str);i++)
+    {
+        if(str[i]>='a'&&str[i]<='z')
+        {
+            if(str[i]==str[i+1])
+            {
+                printf("%c",str[i]);
+                break;
+            }
+        }
+    }
+    return 0;
+}
